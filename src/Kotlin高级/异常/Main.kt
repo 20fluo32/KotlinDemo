48977d0058f65fd95e32be0b4d8315cd67cb64f0
luofan <20fluo@stu.edu.cn>
2024-12-03 15:15:16 +0800

    Kotlin基础学习代码

--- src/Kotlin高级/异常/Main.kt
+++ src/Kotlin高级/异常/Main.kt
@@ -1,5 +1,18 @@
 package Kotlin高级.异常
 
+// 异常基本和java一样，都是继承顶层的Throwable（顶层类型）
 fun main() {
+    // 异常的捕获处理
+    try {
+        throw Exception("出错了")
+        println(1 / 0)
+    } catch (e: Exception) {
+        println(e.message)
+    } finally {
+        println("异常解决")
+    }
 
 }
+
+//重写属性
+class MyException(override var message: String) : Exception(message)
