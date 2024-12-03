5755dbce18448561eca68a54002ae32409eb7029
luofan <20fluo@stu.edu.cn>
2024-12-03 13:39:41 +0800

    Kotlin基础学习代码

--- src/Kotlin高级/官方扩展函数/Main.kt
+++ src/Kotlin高级/官方扩展函数/Main.kt
@@ -1,5 +1,18 @@
 package Kotlin高级.官方扩展函数
 
 fun main() {
+    var a = Any()
+    var b = a.apply {
+        a.toString()
+    }
+}
 
+class Student(var name: String, var age: Int) {
+
+}
+
+//apply的作用就是对某个对象做一些中间操作然后再返回该对象
+fun test(student: Student?): Student? = student?.apply {
+    this.name = "小明"
+    this.age = 18
 }
